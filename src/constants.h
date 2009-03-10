97dc897cb489597a58b396caf973484c2d59f2ed
CPress.Scott <CPress.Scott@79834ff6-061d-11de-b321-f573fdaff03e>
2009-03-10 03:37:50 +0000

    Stable - Added constants.h so that functions.cpp would have access to the *screen.

---
+++ src/constants.h
@@ -0,0 +1,19 @@
+/*
+ * constants.h
+ *
+ *  Created on: Mar 9, 2009
+ *      Author: CPress.Scott
+ */
+
+#ifndef CONSTANTS_H_
+#define CONSTANTS_H_
+
+/*Global Variables*/
+const int SCREEN_WIDTH  = 640;
+const int SCREEN_HEIGHT = 480;
+const int SCREEN_BPP    = 32;
+
+//The screen
+SDL_Surface *screen = NULL;
+
+#endif /* CONSTANTS_H_ */
