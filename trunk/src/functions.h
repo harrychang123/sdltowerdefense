a5350859b57ed3d9b76c7b4d2234e5356b771b67
CPress.Scott <CPress.Scott@79834ff6-061d-11de-b321-f573fdaff03e>
2009-03-10 03:06:25 +0000

    Functions.cpp header file.

---
+++ trunk/src/functions.h
@@ -0,0 +1,22 @@
+/*
+ * functions.h
+ *
+ *  Created on: Mar 9, 2009
+ *      Author: CPress.Scott
+ */
+
+#ifndef FUNCTIONS_H_
+#define FUNCTIONS_H_
+
+#include "SDL/SDL.h"
+#include <string>
+
+std::string intToStr(int);
+bool init();
+bool load_files();
+SDL_Surface* load_image(std::string);
+void apply_surface(int, int, SDL_Surface*, SDL_Surface*, SDL_Rect*);
+void apply_surface(int, int, SDL_Surface*, SDL_Surface*);
+void clean_up();
+
+#endif /* FUNCTIONS_H_ */
