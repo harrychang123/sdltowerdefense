02f7bef6533f39a773b71ad73f2e2dcce2405424
CoreyRuda <CoreyRuda@79834ff6-061d-11de-b321-f573fdaff03e>
2009-03-10 08:28:26 +0000

    -Added GameState.h, which is the GameState class definition -Edited in consatnts.h 	-enum GameState. This has possible game states defined.

--- trunk/src/constants.h
+++ trunk/src/constants.h
@@ -4,6 +4,8 @@
  *  Created on: Mar 9, 2009
  *      Author: CPress.Scott
  */
+//TODO: Add Nessecarry Game Stats as needed to enum GameStates
+//TODO: Change all CREEP_NUM to a set number
 
 #ifndef CONSTANTS_H_
 #define CONSTANTS_H_
@@ -12,7 +14,18 @@
 const int SCREEN_WIDTH     = 640;
 const int SCREEN_HEIGHT    = 480;
 const int SCREEN_BPP       = 32;
-const int CREEP_NUM        = 1; //TODO: Change all CREEP_NUM to a set number
+const int CREEP_NUM        = 1;
+
+//Game State Constants
+enum GameStates
+{
+	STATE_NULL,
+	STATE_INIT,
+	STATE_EXIT,
+	STATE_MENU,
+	STATE_PLAYING,
+	STATE_PAUSED
+};
 
 //The screen
 SDL_Surface *screen = NULL;
