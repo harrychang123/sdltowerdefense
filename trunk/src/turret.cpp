4e03d2fc0ecbcdb4cf222801b8afec2019528845
CPress.Scott <CPress.Scott@79834ff6-061d-11de-b321-f573fdaff03e>
2009-03-10 04:26:22 +0000

    Stable - Fixed bugs where Turret class functions weren't specifying which class they belonged to.

--- trunk/src/turret.cpp
+++ trunk/src/turret.cpp
@@ -23,50 +23,50 @@ Turret::Turret(int x_pos,int y_pos,int tcost,int cost_up,int trange,int tdamage,
 /*****************************************************************************
 Setter Functions
 ******************************************************************************/
-void upgrade()
+void Turret::upgrade()
 {
 	//Set the level of the tower
 	level += 1;
 }
 
-void set_up_cost(int num)
+void Turret::set_up_cost(int num)
 {
 	//Set the cost to upgrade the tower
 	cost_upgrade = num;
 }
 
-void set_y(int num)
+void Turret::set_y(int num)
 {
 	//Set the y value of the turret's position
 	y = num;
 }
 
-void set_x(int num)
+void Turret::set_x(int num)
 {
 	//Set the x value of the turret's position
 	x = num;
 }
 
-void set_cooldown(double num)
+void Turret::set_cooldown(double num)
 {
 	//Set the cooldown of the tower
 	cooldown = num;
 }
 
-void set_cost(int num)
+void Turret::set_cost(int num)
 {
 	//Sets the cost of the tower
 	cost = num;
 }
 
-void set_range(int num)
+void Turret::set_range(int num)
 {
 	//Sets the range of the tower
 	//TODO: Determine how many pixels per int
 	range = num;
 }
 
-void set_damage(int num)
+void Turret::set_damage(int num)
 {
 	//Sets the damage the tower can do
 	damage = num;
@@ -76,49 +76,49 @@ void set_damage(int num)
 Getter Functions
 ******************************************************************************/
 
-int get_cost()
+int Turret::get_cost()
 {
 	//Return the cost of the tower
 	return cost;
 }
 
-int get_range()
+int Turret::get_range()
 {
 	//Return the range of the tower
 	return range;
 }
 
-int get_damage()
+int Turret::get_damage()
 {
 	//Return the damage of the tower
 	return damage;
 }
 
-double get_cooldown()
+double Turret::get_cooldown()
 {
 	//Return the cooldown of the tower
 	return cooldown;
 }
 
-int get_x()
+int Turret::get_x()
 {
 	//Return the x position of the tower
 	return x;
 }
 
-int get_y()
+int Turret::get_y()
 {
 	//Return the y position of the turret
 	return y;
 }
 
-int get_cost_upgrade()
+int Turret::get_cost_upgrade()
 {
 	//Return the cost to upgrade the tower
 	return cost_upgrade;
 }
 
-int get_level()
+int Turret::get_level()
 {
 	//Returns the level of upgrade to the tower
 	//TODO: Determine the max level a tower can be upgraded
